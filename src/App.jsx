a65867b63bcbd6009e702ab12cb5a150135c92b5
Richard Oppong-Konadu <richardok@gmail.com>
2025-03-24 15:47:00 +0000

    add initial folder/page stucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import "./App.scss";
 
-function App() {
-  return <p>Hello World</p>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
