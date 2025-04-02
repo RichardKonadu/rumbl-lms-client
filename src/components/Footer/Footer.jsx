9fb6710b08eba0b798cf8e071912d1821d2b5e09
Richard Oppong-Konadu <richardok@gmail.com>
2025-04-02 17:12:09 +0100

    adjust footer width

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,15 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="footer">
-      <h3 className="footer__title">Rumbl.</h3>
-      <ul className="footer__nav">
-        <li>About</li>
-        <li>Leagues</li>
-        <li>Fixtures</li>
-        <li>My Account</li>
-      </ul>
+      <div className="footer__content">
+        <h3 className="footer__title">Rumbl.</h3>
+        <ul className="footer__nav">
+          <li>About</li>
+          <li>Leagues</li>
+          <li>Fixtures</li>
+          <li>My Account</li>
+        </ul>
+      </div>
     </div>
   );
 }
